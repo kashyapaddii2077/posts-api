b01cb7e1032315a81b0b1244557d822ff1b2e792
Aditya <86510753+babuhacker@users.noreply.github.com>
2026-08-26 12:44:37 +0530

    Initial commit - Posts API

---
+++ posts/views.py
@@ -0,0 +1,59 @@
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter
+from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiTypes
+
+from .models import Post
+from .serializers import PostSerializer
+from .pagination import PostPagination
+
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all().order_by("-created_at")
+    serializer_class = PostSerializer
+
+    filter_backends = [SearchFilter]
+    search_fields = ["title", "body"]
+
+    pagination_class = PostPagination
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="search",
+                type=OpenApiTypes.STR,
+                location=OpenApiParameter.QUERY,
+                description="Search posts by title or body.",
+            ),
+            OpenApiParameter(
+                name="user_id",
+                type=OpenApiTypes.INT,
+                location=OpenApiParameter.QUERY,
+                description="Filter posts by user ID.",
+            ),
+            OpenApiParameter(
+                name="page",
+                type=OpenApiTypes.INT,
+                location=OpenApiParameter.QUERY,
+                description="Page number.",
+            ),
+            OpenApiParameter(
+                name="page_size",
+                type=OpenApiTypes.INT,
+                location=OpenApiParameter.QUERY,
+                description="Number of posts per page.",
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        user_id = self.request.query_params.get("user_id")
+
+        if user_id:
+            queryset = queryset.filter(user_id=user_id)
+
+        return queryset
+    
