9b60462b4fbbb95ad2afa48d748228be4d38876d
Aditya <86510753+babuhacker@users.noreply.github.com>
2026-08-26 13:23:12 +0530

    Configure Redis cache

--- config/settings.py
+++ config/settings.py
@@ -109,7 +109,10 @@ DATABASES = {
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": os.environ.get("REDIS_URL"),
+        "LOCATION": os.environ.get(
+            "REDIS_URL",
+            "redis://127.0.0.1:6379/1"
+        ),
     }
 }
 
